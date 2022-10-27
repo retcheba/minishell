36cf24e0e719695357e66e54229b75ac20f6c3b5
Susanne Brandt <subrandt@3C4.42angouleme.fr>
2022-10-27 17:35:15 +0200

    builtin pwd

---
+++ srcs/builtins/pwd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: subrandt <subrandt@student.42angoulem      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 08:25:07 by subrandt          #+#    #+#             */
+/*   Updated: 2022/10/27 13:24:37 by subrandt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "../minishell.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+
+//Print the absolute pathname of the current working directory.
+//PATH_MAX = 4096 (limits.h)
+void	ft_pwd(char *buf)
+{
+	char	*pwd;
+
+	if (!buf)
+		return ; //ft_error
+	pwd = getcwd(buf, PATH_MAX);
+	printf("%s\n", pwd);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	ft_pwd(argv[1]);
+	return (0);
+}
