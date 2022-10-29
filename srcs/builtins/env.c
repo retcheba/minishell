250848272e642c57e721cf1689f886891a9e1b56
Susanne Brandt <subrandt@1H3.42angouleme.fr>
2022-10-29 14:53:15 +0200

    builtin env

---
+++ srcs/builtins/env.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: subrandt <subrandt@student.42angoulem      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 09:40:11 by subrandt          #+#    #+#             */
+/*   Updated: 2022/10/29 14:49:12 by subrandt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "../minishell.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct s_lst
+{
+	char			*content;
+	struct s_lst	*next;
+}				t_lst;
+
+t_lst	*new_link(void *content);
+t_lst	*ft_lstlast(t_lst *lst);
+t_lst	*ft_lstadd_back(t_lst *lst, t_lst *new);
+void	ft_free_list(t_lst *lst);
+
+void	print_env(t_lst *env)
+{
+	t_lst	*tmp;
+
+	tmp = env;
+	while (tmp)
+	{
+		printf("%s\n", tmp->content);
+		tmp = tmp->next;
+	}
+}
+
+void	ft_env(char **envp, t_lst *env)
+{
+	int		i;
+
+	i = 0;
+	env = NULL;
+	while (envp[i])
+	{
+		if (env == NULL)
+			env = new_link(envp[i]);
+		else
+			ft_lstadd_back(env, new_link(envp[i]));
+		i++;
+	}
+	print_env(env);
+	ft_free_list(env);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_lst	env;
+
+	(void)argc;
+	(void)argv;
+	ft_env(envp, &env);
+	return (0);
+}
